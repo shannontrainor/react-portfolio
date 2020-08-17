88fdf5f61920799acdba9464702ce099ce13e590
Shannon Trainor <sltrainor@outlook.com>
2020-08-16 23:31:38 -0400

    added style

---
+++ reactportfolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const styles = {
+    sectionStyles: {
+        color: "white"
+    }
+}
+
+function About(){
+    return (
+        <section style={styles.sectionStyles} className="section">
+            <h2>About Me</h2>
+        </section>
+    )
+}
